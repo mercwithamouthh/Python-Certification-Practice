1d658ac14f0de83bd5c1883b11f6d1857a587161
mercwithamouthh <lmw012808@gmail.com>
2024-10-24 14:33:34 -0700

    more

--- Python Certification Practice/Question 39.py
+++ Python Certification Practice/Question 39.py
@@ -1,8 +1,8 @@
-distance = float(input("Enter the distance traveled in feet: "))
+distance = int(input("Enter the distance traveled in feet: "))
 
 distance_miles = distance/ 5280    #convert to miles
 
-time = int(input("Enter the time elapsed in seconds: "))
+time = float(input("Enter the time elapsed in seconds: "))
 
 time_hours = time/3600     #convert to hours
 
--- Python Certification Practice/Question 47.py
+++ Python Certification Practice/Question 47.py
@@ -1,1 +1,6 @@
-#answered this but write it down
+#If you want to build a string that reads:
+#Peter's sister's name's "Anna"
+#which of the following literals would you use? (Select all that apply)
+
+print("Peter's sister's name's \"Anna\"")
+print('Peter\'s sister\'s name\'s \"Anna\"')
